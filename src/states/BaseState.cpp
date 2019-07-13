5e1ae41f8aad363d2494575226b98a99de137946
Filmote <simon@bloggingadeadhorse.com>
2019-07-13 16:47:43 +1000

    New game over and pause images.  New HEX

--- src/states/BaseState.cpp
+++ src/states/BaseState.cpp
@@ -72,7 +72,7 @@ void BaseState::renderGameOverOrPause(StateMachine & machine) {
 
   if (gameStats.gameOver) {
 
-    Sprites::drawExternalMask(25, 20, Images::GameOver, Images::GameOver_Mask, 0, 0); 
+    Sprites::drawExternalMask(39, 23, Images::GameOver, Images::GameOver_Mask, 0, 0); 
 
   }
 
@@ -80,7 +80,7 @@ void BaseState::renderGameOverOrPause(StateMachine & machine) {
 
   if (this->paused) {
 
-    Sprites::drawExternalMask(39, 20, Images::Pause, Images::Pause_Mask, 0, 0); 
+    Sprites::drawExternalMask(46, 23, Images::Pause, Images::Pause_Mask, 0, 0); 
 
   }
 
